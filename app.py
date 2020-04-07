c67a5b8a7f81ac4529b58baf9974de0b7812fccc
Harnoor <harnoorqmul@gmail.com>
2020-04-07 15:17:33 +0100

    Added More get request and modified ByCOuntry api to return json value

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
 import requests
 from flask import Flask
+import requests_cache
+requests_cache.install_cache('conv19', backend='sqlite', expire_after=36000)
+
+SUMMARY_JSON = {}
+GLOBAL_JSON = {}
+COUNTRIES_JSON = {}
 
 app = Flask(__name__)
 @app.route('/')
 def hello():
-    return('<h1>Hello World!</h1>')
+    return('<h1>Welcome Conv19 API</h1>')
 
 # make call to external API
 @app.route('/summary', methods=['GET'])
@@ -13,7 +19,30 @@ def get_conv19_summary():
     headers ={}
     payload ={}
     response = requests.request("GET", url, headers=headers, data = payload)
-    return response.json()
+    SUMMARY_JSON = response.json()
+    return SUMMARY_JSON
+
+@app.route('/summary/global', methods=['GET'])
+def get_conv19_summaryGlobal():
+    url = "https://api.covid19api.com/summary"
+    headers ={}
+    payload ={}
+    response = requests.request("GET", url, headers=headers, data = payload)
+    SUMMARY_JSON = response.json()
+    GLOBAL_JSON = SUMMARY_JSON["Global"]
+    return GLOBAL_JSON
+
+@app.route('/summary/country', methods=['GET'])
+def get_conv19_summaryByCountry():
+    url = "https://api.covid19api.com/summary"
+    headers ={}
+    payload ={}
+    response = requests.request("GET", url, headers=headers, data = payload)
+    SUMMARY_JSON = response.json()
+    COUNTRIES_JSON = {"Countries":SUMMARY_JSON["Countries"]}
+    return COUNTRIES_JSON
+
+
 
 
 
---
+++ test.py
@@ -0,0 +1,28 @@
+# import requests
+#
+# url = "https://api.covid19api.com/webhook"
+#
+# payload = "{\"URL\":\"https://covid19api.com/webhook\"}"
+# headers= {}
+#
+# response = requests.request("POST", url, headers=headers, data = payload)
+#
+# print(response.text.encode('utf8'))
+
+import requests
+import json
+
+# r = requests.get('https://api.carbonintensity.org.uk/intensity', params={}, headers=headers)
+url = "https://api.covid19api.com/summary"
+headers ={}
+payload ={}
+response = requests.request("GET", url, headers=headers, data = payload)
+SUMMARY_JSON = response.json()
+print(SUMMARY_JSON.keys())
+a ={"Countries":SUMMARY_JSON["Countries"]}
+print(type(a))
+
+# print(a[:5])
+# print(type(SUMMARY_JSON["Countries"]))
+# print(type(SUMMARY_JSON["Global"]))
+# r.json()
